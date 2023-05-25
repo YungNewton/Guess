760e6649f2f4a91f8c31d37bc52eba2716f30d55
YungNewton <isaacnewtonahanmisi@gmail.com>
2023-05-25 19:19:04 +0100

    commit

---
+++ scripts/NumberGuessingGame.py
@@ -0,0 +1,46 @@
+import random
+import time
+
+print("Welcome to the number guessing game")
+time.sleep(2)
+print('I am thinking of a number between 1 and 100')
+time.sleep(2)
+IsWin = False
+shouldAsk = True
+number = random.randint(1, 100)
+while shouldAsk:
+    level = input("select difficulty level: Type 'easy' or 'Hard' : ")
+    if level.lower() == 'easy' or level.lower() == 'hard':
+        shouldAsk = False
+if level.lower() == 'easy':
+    print("you have 10 attempts")
+    time.sleep(1)
+    for i in range(10):
+        guess = int(input("Guess a number: "))
+        if guess == number:
+            print('CORRECT!! You guessed right')
+            IsWin = True
+            break
+        elif guess < number:
+            print('Too low.')
+            print('Guess again')
+        else:
+            print('Too high')
+            print('guess again')
+else:
+    print("you have 5 attempts")
+    time.sleep(1)
+    for i in range(5):
+        guess = int(input("Guess a number: "))
+        if guess == number:
+            print('CORRECT!! You guessed right')
+            IsWin = True
+            break
+        elif guess < number:
+            print('Too low.')
+            print('Guess again')
+        else:
+            print('Too high')
+            print('guess again')
+if not IsWin:
+    print("'You've run out of attempts!! Try again later.")
